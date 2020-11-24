7bfbaeecaf81da7984f2f5deeb964ece55345264
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-11-24 16:52:14 +0700

    update routes

---
+++ route/routes.go
@@ -0,0 +1,32 @@
+package route
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	db2 "github.com/teten-nugraha/mikro-backend/db"
+	"github.com/teten-nugraha/mikro-backend/injection"
+	"net/http"
+)
+
+
+
+func Init() *echo.Echo {
+
+	db := db2.InitDB()
+	defer db.Close()
+
+	authAPI := injection.InitAuthAPI(db)
+
+	routes := echo.New()
+
+	routes.Use(middleware.Logger())
+
+	routes.GET("/", func(e echo.Context) error {
+		return e.String(http.StatusOK, "Hello from mikro backend API")
+	})
+
+	// login route
+	routes.GET("/generate-hash/:password", authAPI.GenerateHashPassword)
+
+	return routes
+}
