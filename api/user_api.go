05c8fe81d99e48d80a469b350f6aec139eafe062
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-11-25 09:03:13 +0700

    user api

---
+++ api/user_api.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"github.com/labstack/echo"
+	"github.com/teten-nugraha/mikro-backend/service"
+	"net/http"
+	"strconv"
+)
+
+type UserAPI struct {
+	UserService service.UserService
+}
+
+func ProviderUserAPI(u service.UserService) UserAPI {
+	return UserAPI{UserService: u}
+}
+
+func (u *UserAPI) FindById(c echo.Context) error {
+	id := c.Param("id")
+
+	var idInt,_ = strconv.Atoi(id)
+
+	user := u.UserService.FindById(uint(idInt))
+
+	return c.JSON(http.StatusOK, user)
+
+}
