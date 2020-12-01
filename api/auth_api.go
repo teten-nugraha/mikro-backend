bf4738e8d2789ea74630f673e6e02f0f920b0bee
Rifki Rahadian <rrahadian94@gmail.com>
2020-12-01 21:37:30 +0700

    merge

--- api/auth_api.go
+++ api/auth_api.go
@@ -1,10 +1,11 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo"
 	"github.com/teten-nugraha/mikro-backend/helpers"
 	"github.com/teten-nugraha/mikro-backend/service"
-	"net/http"
 )
 
 type AuthAPI struct {
@@ -17,7 +18,7 @@ func ProvideAuthAPI(p service.UserService) AuthAPI {
 	}
 }
 
-func (p *AuthAPI)GenerateHashPassword(c echo.Context) error {
+func (p *AuthAPI) GenerateHashPassword(c echo.Context) error {
 	password := c.Param("password")
 
 	hash, _ := helpers.HashPassword(password)
@@ -47,7 +48,31 @@ func (p *AuthAPI) CheckLogin(c echo.Context) error {
 	}
 
 	return SuccessResponse(c, http.StatusOK, map[string]string{
-		"token":t,
+		"token": t,
 	})
+}
 
+func (p *AuthAPI) Register(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	res, err := p.UserService.CheckLogin(username, password)
+	if err != nil {
+
+		return ErrorResponse(c, http.StatusInternalServerError, err.Error())
+	}
+
+	if !res {
+		return ErrorResponse(c, http.StatusUnauthorized, "Unauthorized")
+	}
+
+	t, err := p.UserService.GenerateToken(username)
+
+	if err != nil {
+		return ErrorResponse(c, http.StatusInternalServerError, err.Error())
+	}
+
+	return SuccessResponse(c, http.StatusOK, map[string]string{
+		"token": t,
+	})
 }
