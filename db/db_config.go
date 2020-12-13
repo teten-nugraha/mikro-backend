58ceb2775a2a2b0b6c5d182e3dbb132be564b840
teten <tennugraha777@gmail.com>
2020-12-13 10:45:44 +0700

    Revert "Revert "rollback prod db config""
    This reverts commit b98427abe82bbdb6e5bed77f09365264c1795c91.

--- db/db_config.go
+++ db/db_config.go
@@ -54,15 +54,14 @@ func processENV(args []string) {
 			}
 			logrus.Info("Mikro Backend using Development DB Profile")
 		}
-	}
-	// else {
-	// 	// ready for production
+	} else {
+		// ready for production
 
-	// 	err := godotenv.Load("production.env")
-	// 	if err != nil {
-	// 		log.Fatal("Error loading production.env file")
-	// 	}
-	// 	logrus.Info("Mikro Backend using Production DB Profile")
-	// }
+		err := godotenv.Load("production.env")
+		if err != nil {
+			log.Fatal("Error loading production.env file")
+		}
+		logrus.Info("Mikro Backend using Production DB Profile")
+	}
 
 }
