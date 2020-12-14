296c2a17a68cf480cdbe8d30cbcef8c28789f2ff
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-12-14 09:02:05 +0700

    add route

--- route/admin_routes.go
+++ route/admin_routes.go
@@ -1,1 +1,19 @@
 package route
+
+import (
+	"github.com/labstack/echo"
+	"github.com/teten-nugraha/mikro-backend/api"
+)
+
+func AdminRoute(routes *echo.Echo, kategoriAPI api.KategoriAPI) {
+
+	admin := routes.Group("/admin")
+	{
+		// Kategori Route
+
+		admin.POST("/kategori", kategoriAPI.SaveOrUpdate)
+		admin.GET("/kategori/:id", kategoriAPI.FindById)
+		admin.GET("/kategori/findByNama", kategoriAPI.FindByName)
+	}
+
+}
